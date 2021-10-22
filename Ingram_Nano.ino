c3517dd324f33addeb068ceb122ea6237aa76244
painnick <painnick@gmail.com>
2021-10-22 22:50:05 +0900

    REFACTORING.

--- Ingram_Nano.ino
+++ Ingram_Nano.ino
@@ -4,60 +4,62 @@
 */
 #include "Siren.h"
 
-#define BUZZER 5
+#define BUZZER_PIN_NO 5
 
-#define LED_HEAD 7  // GREEN & BLUE. 200 ohm
-#define LED_WAIST 8 // YELLOW. 330 ohm
+#define LED_HEAD_PIN_NO 7  // GREEN & BLUE. 200 ohm
+#define LED_WAIST_PIN_NO 8 // YELLOW. 330 ohm
 
-#define LED_SHOULDER1 9  // RED? PNK? 1K ohm
-#define LED_SHOULDER2 10 // RED? PNK? 1K ohm
-#define LED_SHOULDER3 11 // YELLOW. 330 ohm
+#define LED_SHOULDER1_PIN_NO 9  // RED? PNK? 1K ohm
+#define LED_SHOULDER2_PIN_NO 10 // RED? PNK? 1K ohm
+#define LED_SHOULDER3_PIN_NO 11 // YELLOW. 330 ohm
 
 #define SHOULDER_INTERVAL 100  // soulder 1 & 2
 #define SHOULDER3_INTERVAL 300 // soulder 3
 
-bool soulder_on = true;
-bool soulder3_on = true;
+#define INIT_MILLS 10000
 
-Siren siren(BUZZER, false);
+bool soulder_led_on = true;
+bool soulder3_led_on = true;
+
+Siren siren(BUZZER_PIN_NO, false);
 
 void setup()
 {
-  pinMode(BUZZER, OUTPUT);
+  pinMode(BUZZER_PIN_NO, OUTPUT);
 
-  pinMode(LED_HEAD, OUTPUT);
-  pinMode(LED_WAIST, OUTPUT);
+  pinMode(LED_HEAD_PIN_NO, OUTPUT);
+  pinMode(LED_WAIST_PIN_NO, OUTPUT);
 
-  pinMode(LED_SHOULDER1, OUTPUT);
-  pinMode(LED_SHOULDER2, OUTPUT);
-  pinMode(LED_SHOULDER3, OUTPUT);
+  pinMode(LED_SHOULDER1_PIN_NO, OUTPUT);
+  pinMode(LED_SHOULDER2_PIN_NO, OUTPUT);
+  pinMode(LED_SHOULDER3_PIN_NO, OUTPUT);
 
-  digitalWrite(LED_HEAD, HIGH);
-  digitalWrite(LED_WAIST, soulder3_on ? HIGH : LOW);
+  digitalWrite(LED_HEAD_PIN_NO, HIGH);
+  digitalWrite(LED_WAIST_PIN_NO, soulder3_led_on ? HIGH : LOW);
 
-  digitalWrite(LED_SHOULDER1, soulder_on ? HIGH : LOW);
-  digitalWrite(LED_SHOULDER2, soulder_on ? LOW : HIGH);
-  digitalWrite(LED_SHOULDER3, soulder3_on ? HIGH : LOW);
+  digitalWrite(LED_SHOULDER1_PIN_NO, soulder_led_on ? HIGH : LOW);
+  digitalWrite(LED_SHOULDER2_PIN_NO, soulder_led_on ? LOW : HIGH);
+  digitalWrite(LED_SHOULDER3_PIN_NO, soulder3_led_on ? HIGH : LOW);
 }
 
-unsigned long soulder_lastMills = 0;
-unsigned long soulder3_lastMills = 0;
+unsigned long soulder_lastMills = INIT_MILLS;
+unsigned long soulder3_lastMills = INIT_MILLS;
 void loop()
 {
   unsigned long currentMills = millis();
-  if (soulder_lastMills == 0 || soulder_lastMills + SHOULDER_INTERVAL < currentMills)
+  if (soulder_lastMills == INIT_MILLS || soulder_lastMills + SHOULDER_INTERVAL < currentMills)
   {
-    soulder_on = !soulder_on;
-    digitalWrite(LED_SHOULDER1, soulder_on ? HIGH : LOW);
-    digitalWrite(LED_SHOULDER2, soulder_on ? LOW : HIGH);
+    soulder_led_on = !soulder_led_on;
+    digitalWrite(LED_SHOULDER1_PIN_NO, soulder_led_on ? HIGH : LOW);
+    digitalWrite(LED_SHOULDER2_PIN_NO, soulder_led_on ? LOW : HIGH);
 
     soulder_lastMills = currentMills;
   }
-  if (soulder3_lastMills == 0 || soulder3_lastMills + SHOULDER3_INTERVAL < currentMills)
+  if (soulder3_lastMills == INIT_MILLS || soulder3_lastMills + SHOULDER3_INTERVAL < currentMills)
   {
-    soulder3_on = !soulder3_on;
-    digitalWrite(LED_WAIST, soulder3_on ? HIGH : LOW);
-    digitalWrite(LED_SHOULDER3, soulder3_on ? HIGH : LOW);
+    soulder3_led_on = !soulder3_led_on;
+    digitalWrite(LED_WAIST_PIN_NO, soulder3_led_on ? HIGH : LOW);
+    digitalWrite(LED_SHOULDER3_PIN_NO, soulder3_led_on ? HIGH : LOW);
 
     soulder3_lastMills = currentMills;
   }
